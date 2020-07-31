a01b77152edebbcacdf4b405018df9c70d3c3cb9
Phil Zahoruyko <philip.zahoruyko@unity3d.com>
2020-07-31 16:30:57 -0700

    Added batch file to autodetect VS and set its env

---
+++ cmake/SetupVS.bat
@@ -0,0 +1,6 @@
+@echo off
+FOR /F "tokens=* USEBACKQ" %%F IN (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationpath`) DO (
+	SET VS_INSTALL_PATH=%%F
+)
+SET VS_VARS_BAT=%VS_INSTALL_PATH%\VC\Auxiliary\Build\vcvars64.bat
+CALL %VS_VARS_BAT%
